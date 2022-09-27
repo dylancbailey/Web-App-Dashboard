dbd288d139bb4403ad216a8d0d6e253b06822769
Dylan Bailey <wearesleepless@gmail.com>
2022-09-27 14:01:23 -0400

    Adds active class and click event

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const alertBanner = document.getElementById('alert');
+
+//create the html for banner
+alertBanner.innerHTML = 
+`<div class="alert-banner">
+<p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
+to complete</p>
+<p class="alert-banner-close">x</p>
+</div>
+`;
+
+//click x to close
+alertBanner.addEventListener('click', e => {
+    const element = e.target;
+    if (element.classList.contains('alert-banner-close')) {
+        alertBanner.style.display = 'none';
+    }
+});
